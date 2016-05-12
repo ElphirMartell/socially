6e9f0144d804d51b994eb274caae1ce9ef3fcd9b
ElphirMartell <sergioroa2@gmail.com>
2016-05-12 19:34:46 +0200

    init app

---
+++ client/app.ts
@@ -0,0 +1,27 @@
+import 'reflect-metadata';
+import 'zone.js/dist/zone';
+import {Component, NgZone} from 'angular2/core';
+import {bootstrap} from 'angular2/platform/browser';
+import {Parties} from '../collections/parties';
+import {Tracker} from 'meteor/tracker';
+
+ 
+
+@Component({
+  selector: 'app',
+  templateUrl: 'client/app.html'
+})
+
+class Socially {
+  parties:Array;
+
+  constructor(zone:NgZone) {
+    Tracker.autorun(() => zone.run(() => {
+      this.parties = Parties.find().fetch;
+    }));
+  }
+}
+
+
+bootstrap(Socially);
+
